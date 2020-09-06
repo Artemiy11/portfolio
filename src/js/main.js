62d6d8d948c5c35c9743fd8c0143f67b5dba7fa9
Artemy <kartemiy678@gmail.com>
2020-09-06 19:32:20 +0700

    site managed

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,16 @@ hamburger.addEventListener('click', () => {
 close.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__level_percent'),
+      activeLine = document.querySelectorAll('.skills__level_line_active');
+
+counters.forEach((item, i) => {
+    if (item.innerHTML.replace(/\%/, '') > 100) {
+        item.innerHTML = `100%`;
+    } else if (item.innerHTML.replace(/\%/, '') < 0) {
+        item.innerHTML = `0%`;
+    }
+    
+    activeLine[i].style.width = item.innerHTML;
+});
